a5aeecea074ae700170600e3800062609c07b4a3
XPgan <1229308017@qq.com>
2019-10-15 14:52:42 +0800

    上传图片，暂无完成

--- src/page/sub-pages/fast-menu/component/ChangeAvatar.vue
+++ src/page/sub-pages/fast-menu/component/ChangeAvatar.vue
@@ -11,7 +11,7 @@
       <div style="margin-left:20px;">
         <div>上传头像</div>
         <div class="avatar-uploader" style="margin-top:10px;">
-          <el-upload
+          <!-- <el-upload
             action="https://jsonplaceholder.typicode.com/posts/"
             :show-file-list="false"
             :on-success="handleAvatarSuccess"
@@ -19,7 +19,14 @@
           >
             <img v-if="imageUrl" :src="imageUrl" class="avatar" />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-          </el-upload>
+          </el-upload>-->
+          <input
+            class="file-update"
+            type="file"
+            name="image[]"
+            accept="image/*"
+            @change="changefile($event)"
+          />
         </div>
         <div style="margin-top:10px;">
           <el-button type="primary">保存头像</el-button>
@@ -72,7 +79,50 @@ export default {
         this.$message.error("上传头像图片大小不能超过 2MB!");
       }
       return isJPG && isLt2M;
-    }
+    },
+    changefile(e) {
+      let files = e.target.files[0];
+      console.log(files);
+      var reader = new FileReader();
+      //filses就是input[type=file]文件列表，files[0]就是第一个文件，这里就是将选择的第一个图片文件转化为base64的码
+      reader.readAsDataURL(files);
+      let imgStr = "";
+      reader.onload = function(e) {
+        //或者 e.target.result都是一样的，都是base64码
+        imgStr = reader.result.split(",")[1];
+      };
+
+      setTimeout(function() {
+        this.qnToken(imgStr);
+      }, 1000);
+    },
+    qnToken() {
+      //七牛token 七牛云平台安全标识
+      var qiniutoken = "";
+      //七牛云图片base64格式上传地址
+      var uploadUrl = "http://upload-z2.qiniup.com/putb64/-1";
+      //需要上传的图片内容 base64格式
+      var imgStr = "";
+
+      //七牛云平台 配置的域名
+      var urlHeader = "后端给的配置好的域名";
+
+      //前端请求接口回去到后端返回的token
+      // $.ajax({
+      //   url: "xxx",
+      //   type: "get",
+      //   dataType: "json",
+      //   async: false,
+      //   success: function(data) {
+      //     qiniutoken = data.data.token;
+      //   },
+      //   error: function(e) {
+      //     alert(JSON.stringify(e));
+      //   }
+      // });
+      // this.uploadImg()
+    },
+    uploadImg() {}
   }
 };
 </script>
