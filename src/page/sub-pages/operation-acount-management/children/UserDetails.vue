6094ff4ddcfe071df36b5373bde84ae54dbe0890
XPgan <1229308017@qq.com>
2019-11-07 16:58:29 +0800

    平台运营账号管理

---
+++ src/page/sub-pages/operation-acount-management/children/UserDetails.vue
@@ -0,0 +1,89 @@
+<!--新建管理账户-->
+<template>
+  <el-dialog
+    title="用户详情"
+    :visible.sync="dialogVisible"
+    width="60%"
+    class="page"
+    :before-close="handleClose"
+  >
+    <span class="boxTop">
+      <span class="stick"></span>
+      <span style="margin-left:10px;">基本资料</span>
+    </span>
+    <div class="formBox">
+      <el-form :model="ruleForm" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+        <el-form-item label="账户名称 :" prop="name" size="small">
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="账号 :" prop="account" size="small">
+          <el-input v-model="ruleForm.account"></el-input>
+        </el-form-item>
+        <el-form-item label="身份证 :" prop="passWord" size="small">
+          <el-input v-model="ruleForm.passWord"></el-input>
+        </el-form-item>
+        <el-form-item label="联系方式 :" prop="department" size="small">
+          <el-input v-model="ruleForm.department"></el-input>
+        </el-form-item>
+        <el-form-item label="所属部门 :" prop="job" size="small">
+          <el-input v-model="ruleForm.job"></el-input>
+        </el-form-item>
+        <el-form-item label="权限 :" prop="phone" size="small">
+          <el-input v-model="ruleForm.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="账户状态 :" prop="area" size="small">
+          <el-input v-model="ruleForm.area"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: "UserDetails",
+  data() {
+    return {
+      dialogVisible: false,
+      ruleForm: {}
+    };
+  },
+  methods: {
+    handleClose(done) {
+      done();
+    },
+    open(value) {
+      this.ruleForm = value;
+      this.dialogVisible = true;
+    }
+  }
+};
+</script>
+<style scoped>
+.page {
+  min-width: 850px;
+}
+
+.demo-ruleForm {
+  width: 100%;
+}
+
+.el-input {
+  max-width: 350px;
+}
+
+.formBox {
+  width: 470px;
+  margin: 0 auto;
+}
+.stick {
+  height: 25px;
+  width: 5px;
+  background-color: #409eff;
+}
+.boxTop {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
--- src/page/sub-pages/operation-acount-management/normalComponent/AllAccount.vue
+++ src/page/sub-pages/operation-acount-management/normalComponent/AllAccount.vue
@@ -5,7 +5,12 @@
       <div class="searchBox">
         <span>
           <el-input v-model="search.keyWord" placeholder="搜索关键词" style="width:500px;"></el-input>
-          <el-select v-model="search.status" placeholder="全部" style="margin-left:10px;">
+          <el-select
+            v-model="search.status"
+            v-if="type == 0"
+            placeholder="全部"
+            style="margin-left:10px;"
+          >
             <el-option
               v-for="(item,index) in options"
               :key="index"
@@ -26,10 +31,12 @@
         </span>
       </div>
       <div class="file">
-        <el-button type="primary" @click="add">
-          <i class="el-icon-plus" style="color:#fff;"></i>
-          <span style="color:#fff;">新建</span>
-        </el-button>
+        <router-link to="/CreateUserAcount">
+          <el-button type="primary">
+            <i class="el-icon-plus" style="color:#fff;"></i>
+            <span style="color:#fff;">新建</span>
+          </el-button>
+        </router-link>
       </div>
     </div>
     <el-table
@@ -37,41 +44,43 @@
       :data="tableData"
       tooltip-effect="dark"
       style="width: 100%;margin-top:10px;"
-      @selection-change="handleSelectionChange"
       height="600"
       border
       stripe
       :header-cell-style="{background:'rgb(245,245,245)'}"
     >
-      <el-table-column type="selection" width="55"></el-table-column>
       <el-table-column label="账户名称">
         <template slot-scope="scope">
-          <span style="color:#0079fe;">{{scope.row.name}}</span>
+          <span>{{scope.row.name}}</span>
         </template>
       </el-table-column>
       <el-table-column label="账号" show-overflow-tooltip>
         <template slot-scope="scope">
-          <span style="color:#0079fe;">{{scope.row.account}}</span>
+          <span>{{scope.row.account}}</span>
         </template>
       </el-table-column>
       <el-table-column label="身份证" show-overflow-tooltip>
         <template slot-scope="scope">
-          <span style="color:#0079fe;">{{scope.row.managmentArea}}</span>
+          <span>{{scope.row.idcard}}</span>
         </template>
       </el-table-column>
       <el-table-column label="联系方式" show-overflow-tooltip>
         <template slot-scope="scope">
-          <span style="color:#0079fe;">{{scope.row.managmentArea}}</span>
+          <span>{{scope.row.phone}}</span>
         </template>
       </el-table-column>
       <el-table-column label="所属部门" show-overflow-tooltip>
         <template slot-scope="scope">
-          <span style="color:#0079fe;">{{scope.row.managmentArea}}</span>
+          <span>{{scope.row.bm}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="权限" show-overflow-tooltip>
+      <el-table-column label="权限" show-overflow-tooltip min-width="130">
         <template slot-scope="scope">
-          <span style="color:#0079fe;">{{scope.row.managmentArea}}</span>
+          <span>{{scope.row.qx}}</span>
+          <span style="margin-left:10px;" @click="EditPermission(scope.row)">
+            <i class="el-icon-edit" style="color:#0079fe;"></i>
+            <span style="color:#0079fe;">编辑权限</span>
+          </span>
         </template>
       </el-table-column>
       <el-table-column prop="accountStatus" label="账户状态" show-overflow-tooltip>
@@ -88,25 +97,21 @@
         </template>
       </el-table-column>
       <el-table-column prop="lastEdit" label="最后编辑人" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="operating" label="操作" show-overflow-tooltip width="500">
+      <el-table-column prop="operating" label="操作" show-overflow-tooltip min-width="230">
         <template slot-scope="scope">
-          <span style="margin-left:10px;" @click="look(scope.$index, scope.row)">
+          <span style="margin-left:10px;" @click="look(scope.row)">
             <i class="el-icon-chat-line-round" style="color:#0079fe;"></i>
             <span style="color:#0079fe;">查看</span>
           </span>
-          <!-- <span style="margin-left:10px;" @click="openDary(scope.$index, scope.row)">
-            <i class="el-icon-chat-line-round" style="color:#0079fe;"></i>
-            <span style="color:#0079fe;">操作日志</span>
-          </span> -->
-          <span style="margin-left:10px;" @click="thaw(scope.$index, scope.row)">
+          <span style="margin-left:10px;" v-if="scope.row.accountStatus == 1" @click="FreezeAccount(scope.row)">
+            <i class="el-icon-edit" style="color:#0079fe;"></i>
+            <span style="color:#0079fe;">冻结账户</span>
+          </span>
+          <span style="margin-left:10px;" v-if="scope.row.accountStatus == 2" @click="ThawAccount(scope.row)">
             <i class="el-icon-edit" style="color:#0079fe;"></i>
             <span style="color:#0079fe;">解冻账户</span>
           </span>
-          <!-- <span style="margin-left:10px;" @click="edit(scope.$index, scope.row)">
-            <i class="el-icon-edit" style="color:#0079fe;"></i>
-            <span style="color:#0079fe;">编辑</span>
-          </span> -->
-          <span style="margin-left:10px;" @click="remove(scope.$index, scope.row)">
+          <span style="margin-left:10px;" @click="Remove(scope.$index, scope.row)">
             <i class="el-icon-delete" style="color:#0079fe;"></i>
             <span style="color:#0079fe;">删除</span>
           </span>
@@ -115,18 +120,6 @@
     </el-table>
     <div class="underTable">
       <div>
-        <el-button @click="toggleSelection(tableData)">全选&反选</el-button>
-        <el-button @click="toggleSelection()">取消选择</el-button>
-        <el-dropdown style="margin-left:10px;" @command="handleCommand">
-          <el-button style="width:105px;">
-            更多操作
-            <i class="el-icon-arrow-down el-icon--right"></i>
-          </el-button>
-          <el-dropdown-menu>
-            <el-dropdown-item command="1">置顶记录</el-dropdown-item>
-            <el-dropdown-item command="2">删除记录</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
       </div>
       <div>
         <el-pagination
@@ -140,20 +133,22 @@
           :total="search.total"
         ></el-pagination>
       </div>
-      <editDialog ref="editDialog" />
-      <Operationlog ref="Operationlog" />
+      <UserDetails ref="UserDetails" />
     </div>
   </div>
 </template>
 <script>
-import editDialog from "./../editDialog/editDialog";
-import Operationlog from "./../operationlog/Operationlog"; //操作日志
+import UserDetails from "./../children/UserDetails";
 
 export default {
   name: "AllAccount",
   components: {
-    editDialog,
-    Operationlog
+    UserDetails
+  },
+  props: {
+    type: {
+      default: 0
+    }
   },
   data() {
     return {
@@ -165,7 +160,6 @@ export default {
         status: ""
       },
       pagingState: true,
-      value: "更多操作",
       options: [
         {
           value: 1,
@@ -178,10 +172,6 @@ export default {
         {
           value: 3,
           label: "冻结账户"
-        },
-        {
-          value: 4,
-          label: "失效账户"
         }
       ],
       tableData: [
@@ -189,49 +179,46 @@ export default {
           id: 1,
           name: "某某",
           account: "0001",
-          managmentArea: "长沙",
-          accountStatus: 1,
+          idcard: "430521904847711",
           phone: "13657448974",
+          bm: "财务部",
+          qx: "财务",
+          accountStatus: 1,
           lastEdit: "某负责人",
           timeTo: "2019-10-10"
-        }
+        },
+        {
+          id: 2,
+          name: "某某",
+          account: "0001",
+          idcard: "430521904847711",
+          phone: "13657448974",
+          bm: "财务部",
+          qx: "财务",
+          accountStatus: 2,
+          lastEdit: "某负责人",
+          timeTo: "2019-10-10"
+        },
       ],
       multipleSelection: []
     };
   },
-  watch: {},
   methods: {
-    toggleSelection(rows) {
-      if (rows) {
-        rows.forEach(row => {
-          this.$refs.multipleTable.toggleRowSelection(row);
-        });
-      } else {
-        this.$refs.multipleTable.clearSelection();
-      }
+    // 冻结账号
+    FreezeAccount(item){
+
     },
-    // 置顶
-    handleCommand(options) {
-      let selected = this.$refs.multipleTable.selection;
-      let orgin = this.tableData;
-      let last = [];
-      orgin.forEach((item, index) => {
-        selected.forEach((selectedItem, selectedIndex) => {
-          if (item.id == selectedItem.id) {
-            orgin[index] = "";
-          }
-        });
-      });
-      orgin.forEach((item, index) => {
-        if (item != "") {
-          last[last.length] = item;
-        }
-      });
-      if (options == 1) {
-        this.tableData = selected.concat(last);
-      } else if (options == 2) {
-        this.tableData = last;
-      }
+    // 解冻账号
+    ThawAccount(item){
+
+    },
+    // 编辑权限
+    EditPermission(item){
+
+    },
+    //查看用户详情
+    look(item) {
+      this.$refs.UserDetails.open(item);
     },
     // 搜索条件改变
     activeQuery() {
@@ -251,27 +238,14 @@ export default {
         this.search.pageNo = 1;
       }
     },
-    // 新建
-    add() {
-      this.$refs.editDialog.open();
-    },
     // 删除当前项
-    remove(index, row) {
+    Remove(index, row) {
       let orgin = this.tableData;
       orgin.forEach((item, index) => {
         if (item.id == row.id) {
           this.tableData.splice(index, 1);
         }
       });
-    },
-    openDary(index, row) {
-      this.$refs.Operationlog.open();
-    },
-    extend(index, row) {},
-    thaw(index, row) {},
-    edit(index, row) {},
-    handleSelectionChange(val) {
-      this.multipleSelection = val;
     },
     // 分页条件改变
     handleSizeChange(pageSize) {
